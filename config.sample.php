78c23156fa1709dd70c10196be012cf79dd27419
Pierre Granger <p.granger@allier-tourisme.net>
2019-02-20 12:20:59 +0100

    Update config.sample.php

--- config.sample.php
+++ config.sample.php
@@ -1,9 +1,17 @@
 <?php
 
+    /**
+     * Renommer en config.inc.php pour pouvoir utiliser les exemples du dossier /examples
+     */
+
     $config = Array(
         'apiKey'        => 'XXX',
         'projectId'     => 0,
         'baseUrl'       => 'https://api.apidae-tourisme.com/',
         'OAuthClientId' => 'XXX',
         'OAuthSecret'   => 'XXX',
+        'ssoBaseUrl'       => 'https://base.apidae-tourisme.com/',
+		'ssoRedirectUrl'   => '[...]/vendor/sitra-tourisme/sitra-api-php/examples/sso.php', // Remplacer [...] par l'url de votre projet (https:.....)
+		'ssoClientId'      => 'XXX',
+        'ssoSecret'        => 'XXX',
     ) ;
