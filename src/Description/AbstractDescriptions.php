140fdc65a239877d1ebeaeb9ac2b398650eebb6d
Pierre Granger <pierre.granger@apidae-tourisme.com>
2021-12-29 18:37:25 +0100

    Validation par justinrainbow/json-schema et plus manuelle

--- src/Description/AbstractDescriptions.php
+++ src/Description/AbstractDescriptions.php
@@ -13,17 +13,14 @@ abstract class AbstractDescriptions
     public static function encodeQuery(string|array $data, string $operation): string
     {
         $dataObject = null;
-        $dataArray = null;
         $dataJson = null;
 
         if (is_array($data)) {
-            $dataArray = $data;
             $dataJson = json_encode($data);
             $dataObject = json_decode($dataJson);
         } else {
             $dataJson = $data;
             $dataObject = json_decode($dataJson);
-            $dataArray = json_decode($data, true);
             if (json_last_error() !== JSON_ERROR_NONE)
                 throw new \Exception('\'query\' parameter is not a valid json string : ' . $data);
         }
@@ -40,75 +37,9 @@ abstract class AbstractDescriptions
                 }
                 throw new \Exception($exceptionMessage);
             }
-            //static::checkData(static::$schemas[$operation], $data, 'query[]', $operation);
-
         }
 
         return json_encode($data);
-    }
-
-    /**
-     * @var array $schema Propriété décrite par le schema
-     * @var array $value Valeurs passées par l'utilisateur dans sa requête
-     * @var string $parent Nom du noeud parent testé (pour afficher des messages clairs types : Erreur sur la valeur e query[center[coordinates]]...)
-     * @var string $operation Nom de l'opération d'origine (getReferenceCity...) pour afficher des messages clairs (Erreur sur l'opération getReferenceCity)
-     */
-    private static function checkData(array $schema, array $data, string $parent, string $operation): void
-    {
-        $properties = $schema['properties'];
-        $propertiesNames = array_keys($schema['properties']);
-        /**
-         * On commence par parcourir toutes les valeurs envoyées dans la requête
-         * ex : query={'communeIds':[1,2,3],...}
-         * $k = 'communeIds', $v = [1,2,3]
-         */
-
-        $dataNames = array_keys($data);
-        $diff = array_diff($dataNames, $propertiesNames);
-        $union = array_merge($dataNames, $propertiesNames);
-
-        if (
-            isset($schema['additionalProperties'])
-            && $schema['additionalProperties'] == false
-            && sizeof($diff) > 0
-        )
-            throw new \Exception('Parameter(s) ' . implode(', ', $diff) . ' in ' . $parent . ' not allowed in ' . $operation . ' (not in ' . implode(', ', $propertiesNames) . ')');
-
-        foreach ($union as $parameterName) {
-
-            unset($property);
-            if (isset($properties[$parameterName])) $property = $properties[$parameterName];
-            /** La propriété n'existe pas, il s'agit d'un paramètre additionnel autorisé ajouté par l'utilisateur, rien à vérifier */
-            else continue;
-
-            unset($userValue);
-            if (isset($values[$parameterName])) $userValue = $values[$parameterName];
-
-            //if ($property['type'] == 'object' && $property[])
-
-            if ($property['type'] == 'array') {
-                if (!is_array($v))
-                    throw new \Exception('Parameter query[' . $k . '] should be an array');
-                if ($prop['items']['type'] == 'integer') {
-                    if (sizeof(array_filter($v, function ($v) {
-                        return !is_integer($v);
-                    })) > 0)
-                        throw new \Exception('Values of array query[' . $k . '] should be integers');
-                }
-                if ($prop['items']['type'] == 'string') {
-                    if (sizeof(array_filter($v, function ($v) {
-                        return !is_string($v);
-                    })) > 0)
-                        throw new \Exception('Values of array query[' . $k . '] should be string');
-                }
-            } elseif ($prop['type'] == 'integer' && !is_integer($v))
-                throw new \Exception('Parameter query[' . $k . '] should be an integer');
-            elseif ($prop['type'] == 'string' && !is_string($v))
-                throw new \Exception('Parameter query[' . $k . '] should be a string');
-
-            if (isset($prop['enum']) && !in_array($v, $prop['enum']))
-                throw new \Exception('Value ' . $v . ' in query[' . $k . '] is not allowed (not in ' . implode(', ', $prop['enum']) . ')');
-        }
     }
 
     private static function loadSchemas()
