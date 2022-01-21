c36d41e75ce66a5fa1e9efd7d9cd3e4becb35fed
root <root@a23b518799af>
2022-01-21 17:05:43 +0100

    Ajout tests fonctionnels

--- tests/functional/AgendaTest.php
+++ tests/functional/AgendaTest.php
@@ -16,21 +16,48 @@ class AgendaTest extends FuncBase
 {
     public function testsearchAgenda()
     {
-        $this->markTestIncomplete();
+        $result = $this->client->agendaSimpleListObjetsTouristiques(['query' => ['searchQuery' => 'vélo', 'count' => 61, 'responseFields' => ['nom', 'localisation']]]);
+        $this->assertEquals('v002', $result['formatVersion']);
+        $this->assertEquals('vélo', $result['query']['searchQuery']);
+        $this->assertEquals(61, $result['query']['count']);
+        $this->assertEquals(61, sizeof($result['objetsTouristiques']));
+        $this->assertTrue($result['numFound'] > 400);
+        $this->assertTrue(isset($result['objetsTouristiques'][0]['type']));
+        $this->assertTrue(isset($result['objetsTouristiques'][0]['localisation']['adresse']['commune']));
     }
 
     public function testsearchAgendaIdentifier()
     {
-        $this->markTestIncomplete();
+        $result = $this->client->agendaSimpleListIdentifiants(['query' => ['searchQuery' => 'vélo', 'count' => 61, 'responseFields' => ['nom', 'localisation']]]);
+        $this->assertEquals('v002', $result['formatVersion']);
+        $this->assertEquals('vélo', $result['query']['searchQuery']);
+        $this->assertEquals(61, $result['query']['count']);
+        $this->assertEquals(61, sizeof($result['objetTouristiqueIds']));
+        $this->assertTrue($result['numFound'] > 400);
+        $this->assertTrue(sizeof($result['objetTouristiqueIds']) == sizeof(array_filter($result['objetTouristiqueIds'], 'is_int')));
     }
 
     public function testsearchDetailedAgenda()
     {
-        $this->markTestIncomplete();
+        $result = $this->client->agendaDetailleListObjetsTouristiques(['query' => ['searchQuery' => 'vélo', 'count' => 61, 'responseFields' => ['nom', 'localisation']]]);
+        $this->assertEquals('vélo', $result['query']['searchQuery']);
+        $this->assertEquals(61, $result['query']['count']);
+        $this->assertTrue($result['numFound'] > 400);
+        $keys = array_keys($result['objetsTouristiques']);
+        $first_date = array_shift($keys);
+        $this->assertTrue(isset($result['objetsTouristiques'][$first_date][0]['type']));
+        $this->assertTrue(isset($result['objetsTouristiques'][$first_date][0]['localisation']['adresse']['commune']));
     }
 
     public function testsearchDetailedAgendaIdentifierr()
     {
-        $this->markTestIncomplete();
+        $result = $this->client->agendaDetailleListIdentifiants(['query' => ['searchQuery' => 'vélo', 'count' => 61, 'responseFields' => ['nom', 'localisation']]]);
+        $this->assertEquals('vélo', $result['query']['searchQuery']);
+        $this->assertEquals(61, $result['query']['count']);
+        $this->assertTrue($result['numFound'] > 400);
+        $keys = array_keys($result['objetTouristiqueIds']);
+        $first_date = array_shift($keys);
+        $this->assertMatchesRegularExpression('#^[0-9]{4}-[0-9]{2}-[0-9]{2}$#', $first_date);
+        $this->assertEquals(sizeof($result['objetTouristiqueIds'][$first_date]), sizeof(array_filter($result['objetTouristiqueIds'][$first_date], 'is_int')));
     }
 }
--- tests/functional/TouristicObjectsTest.php
+++ tests/functional/TouristicObjectsTest.php
@@ -7,29 +7,56 @@
 
 namespace ApidaePHP\Tests\Functional;
 
-use ApidaePHP\Tests\Functional\FuncBase;
 use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Command\Result;
+use ApidaePHP\Tests\Functional\FuncBase;
 
 class TouristicObjectsTest extends FuncBase
 {
-    public function testgetObjectById()
+    /**
+     * @testdox Sérialisation JSON
+     * --dataProvider objetDemoSimple
+     */
+    public function testObjetSimple()
     {
-        $methods = ['getObjectById', 'objetTouristiqueGetById'];
-        foreach ($methods as $method) {
-            /** @var Result $result */
-            $result = $this->client->$method(['id' => 1234]);
-            $request = $this->client->getLastRequest();
-            //$this->assertEquals(200, $result['response']->getStatusCode());
-            //$this->assertEquals([], $transaction['request']->query);
-        }
-        $this->markTestIncomplete();
+        /** @var Result $objetDemo */
+        $objetDemo = $this->client->objetTouristiqueGetById(['id' => 868850, 'responseFields' => 'nom', 'locales' => 'fr,en']); //  __ TEST Commerces et Services
+        $this->assertArrayHasKey('libelleEn', $objetDemo['nom']);
+        $this->assertFalse(isset($objetDemo['localisation']));
     }
 
-    public function testgetObjectByIdentifier()
+    /**
+     * @testdox Sérialisation JSON
+     * --dataProvider objetDemoComplet
+     */
+    public function testObjetComplet()
     {
-        $methods = ['getObjectByIdentifier', 'objetTouristiqueGetByIdentifier'];
-        foreach ($methods as $method) {
-        }
-        $this->markTestIncomplete();
+        /** @var Result $objetDemo */
+        $objetDemo = $this->client->objetTouristiqueGetById(['id' => 868850, 'responseFields' => '@all', 'locales' => 'fr,en']); //  __ TEST Commerces et Services
+
+        $this->assertArrayHasKey('aspects', $objetDemo);
+        $this->assertIsArray($objetDemo['aspects']);
+
+        $this->assertArrayHasKey('metadonnees', $objetDemo);
+        $this->assertIsArray($objetDemo['metadonnees']);
+        $this->assertEquals('idProduct', $objetDemo['metadonnees'][0]['contenus'][0]['metadonnee']['idProduct']);
+
+        $this->assertArrayHasKey('descriptifsThematises', $objetDemo['presentation']);
+        $this->assertIsArray($objetDemo['presentation']['descriptifsThematises']);
+        $this->assertArrayHasKey('theme', $objetDemo['presentation']['descriptifsThematises'][0]);
+        $this->assertArrayHasKey('id', $objetDemo['presentation']['descriptifsThematises'][0]['theme']);
+        $this->assertArrayHasKey('description', $objetDemo['presentation']['descriptifsThematises'][0]);
     }
+
+    /*
+    public function objetDemoSimple()
+    {
+        return $this->client->objetTouristiqueGetById(['id' => 868850, 'responseFields' => 'nom', 'locales' => 'fr,en']); //  __ TEST Commerces et Services
+    }
+
+    public function objetDemoComplet()
+    {
+        return $this->client->objetTouristiqueGetById(['id' => 868850, 'responseFields' => '@all', 'locales' => 'fr,en']); //  __ TEST Commerces et Services
+    }
+    */
 }
